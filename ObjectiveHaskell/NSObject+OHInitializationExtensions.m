5fdc74df40edb62fb10519f8109cb085bafdb1ab
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-15 12:47:38 -0700

    Disable retrieval of command-line arguments on iOS

--- ObjectiveHaskell/NSObject+OHInitializationExtensions.m
+++ ObjectiveHaskell/NSObject+OHInitializationExtensions.m
@@ -6,7 +6,9 @@
 //  Released into the public domain.
 //
 
+#if !__IPHONE_OS_VERSION_MAX_ALLOWED
 #import <crt_externs.h>
+#endif
 
 @interface NSObject (OHInitializationExtensions)
 @end
@@ -18,7 +20,12 @@
 @implementation NSObject (OHInitializationExtensions)
 
 + (void)load {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED
+    // We don't support retrieving command-line arguments on iOS
+    hs_init(NULL, NULL);
+#else
     hs_init(_NSGetArgc(), _NSGetArgv());
+#endif
 
     // Shut down the Haskell runtime upon exit
     atexit(&hs_exit);
